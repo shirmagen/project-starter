5558ea16fef0375fdec28d62e6834f7084dae927
Shir Magen <shirmagen095@gmail.com>
2021-10-30 21:30:39 +0300

    remove(redundant-files)

--- services/stock-action/stock-action.service.js
+++
@@ -1,8 +0,0 @@
-import { create as createTask } from '../task/task.service';
-import { statuses } from '../../utils/task-status.enum';
-
-export const actionOnStock = async ({ product, validator, actionText }) => {
-  if (validator) validator({ product });
-
-  await createTask({ action: actionText, product, location, status: statuses.waiting });
-};
